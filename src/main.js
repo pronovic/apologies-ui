64e59c9576363b3c58830cec6a3de900ab0f8322
Kenneth Pronovici <ken.pronovici@objectpartners.com>
2020-05-22 11:39:07 -0500

    Remove unused eslint-disable statements

--- src/main.js
+++ src/main.js
@@ -5,10 +5,8 @@ import store from './store'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
     router,
     store,
     render: (h) => h(App),
 }).$mount('#app')
-/* eslint-enable no-new */
