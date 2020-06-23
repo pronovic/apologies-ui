1bc0250649b5c148c503d15273cc53ca954d92c1
Kenneth Pronovici <ken.pronovici@objectpartners.com>
2020-06-22 20:44:24 -0500

    Create real unit test for StartGameButton.vue

--- __tests__/components/StartGameButton.test.js
+++ __tests__/components/StartGameButton.test.js
@@ -1,6 +1,19 @@
-import { mount } from '@vue/test-utils'
+import Vue from 'vue'
+import { createLocalVue, mount } from '@vue/test-utils'
+import BootstrapVue, { BButton, BIcon } from 'bootstrap-vue'
+
 import { Store } from 'vuex-mock-store'
+
+import * as client from 'Utils/client'
+
 import StartGameButton from 'Components/StartGameButton.vue'
+
+const localVue = createLocalVue()
+localVue.use(BootstrapVue)
+localVue.component('b-button', BButton)
+localVue.component('b-icon', BIcon)
+
+jest.mock('Utils/client')
 
 const store = new Store({
     state: {},
@@ -15,9 +28,59 @@ const mocks = {
     $store: store,
 }
 
-describe('Component', () => {
-    test('is a Vue instance', () => {
-        const wrapper = mount(StartGameButton, { mocks })
-        expect(wrapper.exists()).toBeTruthy()
+describe('Components/StartGameButton.vue', () => {
+    let wrapper
+
+    beforeEach(() => {
+        const div = document.createElement('div')
+        document.body.appendChild(div)
+        wrapper = mount(StartGameButton, { localVue, mocks, attachTo: div })
+    })
+
+    afterEach(() => {
+        store.reset()
+        wrapper.destroy()
+    })
+
+    test('no game advertised == no button', async () => {
+        store.getters.isGameAdvertised = false
+        store.getters.isGameStarted = false
+        store.getters.isGameCompleted = false
+        await Vue.nextTick()
+        expect(wrapper.findComponent({ ref: 'button' }).exists()).toBe(false)
+    })
+
+    test('game advertised == button', async () => {
+        store.getters.isGameAdvertised = true
+        store.getters.isGameStarted = false
+        store.getters.isGameCompleted = false
+        await Vue.nextTick()
+        expect(wrapper.findComponent({ ref: 'button' }).exists()).toBe(true)
+    })
+
+    test('game started == no button', async () => {
+        store.getters.isGameAdvertised = true
+        store.getters.isGameStarted = true
+        store.getters.isGameCompleted = false
+        await Vue.nextTick()
+        expect(wrapper.findComponent({ ref: 'button' }).exists()).toBe(false)
+    })
+
+    test('game completed == no button', async () => {
+        store.getters.isGameAdvertised = true
+        store.getters.isGameStarted = false
+        store.getters.isGameCompleted = true
+        await Vue.nextTick()
+        expect(wrapper.findComponent({ ref: 'button' }).exists()).toBe(false)
+    })
+
+    test('click starts game', async () => {
+        store.getters.isGameAdvertised = true
+        store.getters.isGameStarted = false
+        store.getters.isGameCompleted = false
+        await Vue.nextTick()
+
+        wrapper.findComponent({ ref: 'button' }).trigger('click')
+        expect(client.startGame).toHaveBeenCalled()
     })
 })
