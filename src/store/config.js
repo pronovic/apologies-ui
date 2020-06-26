d9b996554e2f976d8e07751c2014f69d4f44a9a6
Kenneth Pronovici <ken.pronovici@objectpartners.com>
2020-06-26 09:29:05 -0500

    Add config notes

--- src/store/config.js
+++ src/store/config.js
@@ -1,11 +1,16 @@
 // Callers should access configuration through the store, not directly from here.
 //
-// Although parcel loads process.env for us based on the .env file, that variable
-// is not available to Vue components.  We need to map the process.env variables
-// into something that is available to Vue.  This is repetitive, but seems to be
-// our best option.  We can't just iterate through process.env, because it exposes
-// all environment variables, and that's considered a security risk.  We need to
-// list specific variables individually.
+// Although parcel loads process.env for us based on the .env file, that
+// variable is not available to Vue components.  We need to map the process.env
+// variables into something that is available to Vue.  This is repetitive, but
+// seems to be our best option.
+//
+// We aren't allowed to just iterate through process.env, because it exposes
+// all environment variables, and that's considered a security risk.  We need
+// to list specific variables individually.  Also, it doens't work to make this
+// function accept an env argument, and pass process.env into it.  I think
+// there is some sort of compile time search/replace going on and process.env
+// isn't really a variable in the normal sense.
 
 function loadConfig() {
     return {
